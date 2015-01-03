9ceb0336e7a1db6835c15eaf891bafddf7d25831
linxichen <linxichen88@gmail.com>
2015-01-03 17:07:24 -0500

    VFI done. Need to check accuracy and sense.

---
+++ investment_goods/deep_para.m
@@ -0,0 +1,10 @@
+bbeta = 0.996;
+ttau = 0.00; % search cost
+aalpha = 0.3; % y = a*k^aalpha*l^v
+v = 0.6;
+aalpha0 = 1.25; % search elasticity
+ddelta = 0.02;
+pphi = 0.0000001; % price of disinvestment relative to investment
+rrhox = 0.98;
+ssigmax = 0.08;
+ppsi = 0; % quadratic cost of investment adjustment
