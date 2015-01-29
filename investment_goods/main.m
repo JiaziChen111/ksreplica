b53bba6d33a42e7c1ab9f7180bb1b9a71342745c
Linxi Chen <linxichen@Linxis-MacBook-Pro.local>
2015-01-28 22:33:16 -0500

    Sloppily running version of our own algorithm.

--- investment_goods/main.m
+++ investment_goods/main.m
@@ -3,12 +3,12 @@ clear; close all; clc;
 deep_para; % load parameters
 
 %% Accuracy control 
-nk = 250; % number of grid points on capital stock
-nx = 11; % number of grid points on idiosyncractic prod.
-nK = 250; % ... on aggregate capital level
+nk = 50; % number of grid points on capital stock
+nx = 7; % number of grid points on idiosyncractic prod.
+nK = 50; % ... on aggregate capital level
 m = 3; % support is m s.d. away from mean
-nq = 30;
-tol = 1e-4;
+nq = 10;
+tol = 1e-2;
 damp = 0.5;
 
 %% Grids
@@ -16,12 +16,12 @@ damp = 0.5;
 % Pssigma = 0.5*ones(2,2);
 [X,PX] = tauchen(nx,0,rrhox,ssigmax,m);
 X = exp(X); % Convert back to level
-K = linspace(0.2,100,nk)'; % calibrated from ig_calibration_10_4
+K = linspace(0.2,20,nk)'; % calibrated from ig_calibration_10_4
 w = 3.63; % wage, taken as given, calibrated from ig_calibration_10_4
 
 %% Pretend everything else is solved
 q_old = ones(nK,1);
-q_grid = linspace(0.1,10,nq);
+q_grid = linspace(0.1,1,nq);
 pphi_tthetaC = log(0.1);
 pphi_tthetaK = 0;
 ttheta_old = q_old;
@@ -40,12 +40,25 @@ V_old = ones(nk,nx,nK); % maximized value after discrete choice
 V_new = ones(nk,nx,nK); % 
 
 profit = zeros(nk,nx); % Compute operation profit resulting from labor choice
+L = zeros(nk,nx);
+% for i_k = 1:nk
+%     for i_x = 1:nx
+%         profit(i_k,i_x) = K(i_k)*X(i_x)^(1/aalpha)*w^((aalpha-1)/aalpha)*(1-aalpha)^(1/aalpha)*(aalpha/(1-aalpha));
+%     end
+% end
+nu = 0.5; % y = x*k^aalpha*L^nu
 for i_k = 1:nk
     for i_x = 1:nx
-        profit(i_k,i_x) = K(i_k)*X(i_x)^(1/aalpha)*w^((aalpha-1)/aalpha)*(1-aalpha)^(1/aalpha)*(aalpha/(1-aalpha));
+        L(i_k,i_x) = (X(i_x)*nu*K(i_k)^aalpha/w)^(1/(1-nu));
+        profit(i_k,i_x) = X(i_x)*K(i_k)^aalpha*L(i_k,i_x)^nu-L(i_k,i_x)*w;
     end
 end
 
+%% Main Body of KS iter
+diff = 10;
+outer_iter = 0;
+while diff > tol
+% Start VFI here
 err = 10;
 iter = 0;
 while err > tol
@@ -60,7 +73,8 @@ while err > tol
             for i_k = 1:nk
                 [~,i_k_noinvest] = min(abs(K-(1-ddelta)*K(i_k)));
                 U_new(i_k,i_x,i_K) = profit(i_k,i_x) + bbeta*EV(i_k_noinvest,i_x);
-                moving_part = bbeta*EV(:,i_x) - q*K; % Only two terms in W is moving with k_t+1
+                % moving_part = bbeta*EV(:,i_x) - q*K; % Only two terms in W is moving with k_t+1
+                moving_part = bbeta*EV(:,i_x) - q*K.*(K>=K(i_k))-.3*q*K.*(K<K(i_k));
                 [~,i_k_investopt] = max(moving_part);
                 W_new(i_k,i_x,i_K) = profit(i_k,i_x) -mmu*q*(K(i_k_investopt)-(1-ddelta)*K(i_k)) - ttau + bbeta*(mmu*EV(i_k_investopt,i_x)+(1-mmu)*EV(i_k_noinvest,i_x));
             end
@@ -77,7 +91,7 @@ while err > tol
     disp(err);
 end
 
-%% When converged, find policy functions
+% When converged, find policy functions
 kopt = zeros(nk,nx,nK);
 koptind = zeros(nk,nx,nK);
 active = zeros(nk,nx,nK);
@@ -93,7 +107,7 @@ for i_K = 1:nK
         for i_k = 1:nk
             [~,i_k_noinvest] = min(abs(K-(1-ddelta)*K(i_k)));
             U_new(i_k,i_x,i_K) = profit(i_k,i_x) + bbeta*EV(i_k_noinvest,i_x);
-            moving_part = bbeta*EV(:,i_x) - q*K; % Only two terms in W is moving with k_t+1
+            moving_part = bbeta*EV(:,i_x) - q*K.*(K>=(1-ddelta).*K(i_k))+.3*q*K.*(K<(1-ddelta).*K(i_k)); % Only two terms in W is moving with k_t+1
             [~,i_k_investopt] = max(moving_part);
             W_new(i_k,i_x,i_K) = profit(i_k,i_x) -mmu*q*(K(i_k_investopt)-(1-ddelta)*K(i_k)) - ttau + bbeta*(mmu*EV(i_k_investopt,i_x)+(1-mmu)*EV(i_k_noinvest,i_x));
             if W_new(i_k,i_x,i_K) > U_new(i_k,i_x,i_K)
@@ -109,7 +123,7 @@ for i_K = 1:nK
     end    
 end
 
-%% Once we have the policy, find the optimal q policy assuming buyers believe "same old" happens tomorow and so on
+% Once we have the policy, find the optimal q policy assuming buyers believe "same old" happens tomorow and so on
 revenue = zeros(nq,1);
 for i_K = 1:nK
     for i_q = 1:nq
@@ -126,13 +140,17 @@ for i_K = 1:nK
             for i_k = 1:nk
                 [~,i_k_noinvest] = min(abs(K-(1-ddelta)*K(i_k)));
                 U_new(i_k,i_x,i_K) = profit(i_k,i_x) + bbeta*EV(i_k_noinvest,i_x);
-                moving_part = bbeta*EV(:,i_x) - q*K; % Only two terms in W is moving with k_t+1
+                moving_part = bbeta*EV(:,i_x) - q*K.*(K>=(1-ddelta).*K(i_k))+.3*q*K.*(K<(1-ddelta).*K(i_k)); % Only two terms in W is moving with k_t+1
                 [~,i_k_investopt] = max(moving_part);
                 W_new(i_k,i_x,i_K) = profit(i_k,i_x) -mmu*q*(K(i_k_investopt)-(1-ddelta)*K(i_k)) - ttau + bbeta*(mmu*EV(i_k_investopt,i_x)+(1-mmu)*EV(i_k_noinvest,i_x));
                 if W_new(i_k,i_x,i_K) > U_new(i_k,i_x,i_K)
                     kopt(i_k,i_x,i_K) = K(i_k_investopt);
                     koptind(i_k,i_x,i_K) = i_k_investopt;
-                    active(i_k,i_x,i_K) = 1;
+                    if K(i_k_investopt)>=K(i_k) % invest
+                        active(i_k,i_x,i_K) = 1;
+                    else
+                        active(i_k,i_x,i_K) = 1; % disinvest
+                    end
                 else
                     kopt(i_k,i_x,i_K) = K(i_k_noinvest);
                     koptind(i_k,i_x,i_K) = i_k_noinvest;
@@ -140,10 +158,9 @@ for i_K = 1:nK
                 end
                 
                 % Compute revenue 
-                revenue(i_q) = revenue(i_q) + active(i_k,i_x,i_K)*q*(kopt(i_k,i_x,i_K)-(1-ddelta)*K(i_k));
+                revenue(i_q) = revenue(i_q) + abs(active(i_k,i_x,i_K))*q*(kopt(i_k,i_x,i_K)-(1-ddelta)*K(i_k));
             end
         end
-
     end
     [~,i_q_opt] = max(revenue);
     q_old(i_K) = q_grid(i_q_opt);
@@ -152,11 +169,12 @@ end
 save main.mat
 
 %% Given individual policies, simulate a large panel to update aggregate law of motion
-T = 1000;
-N = 1000;
+T = 2000;
+N = 2000;
+burnin = 500;
 kss = 10;
 Ksim = kss*ones(1,T);
-tthetasim = zeros(1,T);
+tthetasim = exp(pphi_tthetaC+pphi_tthetaK*log(kss))*ones(1,T);
 
 dist_k_now = kss*ones(N,1);
 dist_k_tmr = dist_k_now;
@@ -185,7 +203,7 @@ for t = 1:T
     end
     
     % Find the ttheta, the measure of active firms as a function of agg. K
-    tthetasim(t) = sum(activesim);
+    tthetasim(t) = sum(activesim)/N;
     
     % Get ready for next period
     dist_k_now = dist_k_tmr;
@@ -196,13 +214,24 @@ for t = 1:T
 end
 
 % Regress to get coefficients
-X = [ones(T-1,1) log(Ksim(1:T-1))'];
-Y = log(Ksim(2:T)');
-bbeta = (X'*X)\(X'*Y);
-pphi_KC_new = damp*bbeta(1)+(1-damp)*pphi_KC; pphi_KK_new = damp*bbeta(2)+(1-damp)*pphi_KK;
+X = [ones(T-burnin-1,1) log(Ksim(burnin+1:T-1))'];
+Y = log(Ksim(2+burnin:T)');
+bbeta_K = (X'*X)\(X'*Y);
+pphi_KC_new = damp*bbeta_K(1)+(1-damp)*pphi_KC; pphi_KK_new = damp*bbeta_K(2)+(1-damp)*pphi_KK;
 
-Y = log(tthetasim(2:T))';
-bbeta = (X'*X)\(X'*Y);
-pphi_tthetaC_new = damp*bbeta(1)+(1-damp)*pphi_tthetaC; pphi_tthetaK_new = damp*bbeta(2)+(1-damp)*pphi_tthetaK;
+Y = log(tthetasim(2+burnin:T)+1e-10)';
+bbeta_ttheta = (X'*X)\(X'*Y);
+pphi_tthetaC_new = damp*bbeta_ttheta(1)+(1-damp)*pphi_tthetaC; pphi_tthetaK_new = damp*bbeta_ttheta(2)+(1-damp)*pphi_tthetaK;
 
 diff = norm([pphi_KC,pphi_KK,pphi_tthetaC,pphi_tthetaK]-[pphi_KC_new,pphi_KK_new,pphi_tthetaC_new,pphi_tthetaK_new],Inf);
+
+% Update mmu_old as well
+pphi_tthetaC = pphi_tthetaC_new; pphi_tthetaK = pphi_tthetaK_new;
+for i_K = 1:nK
+    ttheta_old(i_K) = exp(pphi_tthetaC+pphi_tthetaK*log(K(i_K)));
+end
+mmu_old = 1./(1+ttheta_old.^(-aalpha0)).^(1/aalpha0);
+pphi_KC = pphi_KC_new; pphi_KK = pphi_KK_new;
+
+outer_iter = outer_iter + 1;
+end
